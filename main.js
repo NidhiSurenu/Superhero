5dc848db108eaa2eb6215dea9ac7bf290ed5edd2
NidhiSurenu <81507816+NidhiSurenu@users.noreply.github.com>
2021-04-06 12:58:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -83,3 +83,23 @@ if(keypressed=='37')
     console.log("left");
 }
 }
+function up(){
+    if(player_y>=0){player_y=player_y-block_height;
+    canvas.remove(player_object);
+player_update();}
+}
+function down(){
+    if(player_y<=450){player_y=player_y+block_height;
+    canvas.remove(player_object);
+player_update();}
+}
+function left(){
+    if(player_x>=0){player_x=player_x-block_width;
+    canvas.remove(player_object);
+player_update();}
+}
+function right(){
+    if(player_x<=850){player_x=player_x+block_width;
+    canvas.remove(player_object);
+player_update();}
+}
